88cf4f17dcde825f19fca8ce9f388531ea93641e
aidenzegil <40921524+aidenzegil@users.noreply.github.com>
2022-05-24 17:19:08 -0400

    fully functional

--- app/api/tweet_gen_routes.py
+++ app/api/tweet_gen_routes.py
@@ -19,16 +19,17 @@ router = APIRouter(
 
 
 @router.post("/")
-# async def generate(thread: str = Body(embed=True), numberOfThreads: int = Body(embed=True)):
-#     final_prompt = json.dumps({"Continue " + {thread} + "in " + {str(numberOfThreads)} + "point. The points can be a maximum of 280 characters. Points that approach this maximum are preferred."})
-#     response = openai.Completion.create(
-#     engine="text-davinci-002",
-#     prompt= final_prompt,
-#     temperature=1,
-#     max_tokens= (numberOfThreads * 70),
-#     top_p=1,
-#     frequency_penalty=1.5,
-#     presence_penalty=1.5
-# )
-async def generate():
-    return ("-Did you know that the globe is divided into 24 time zones? \n-Or that there are 196 countries in the world? \n-How about that Antarctica is almost twice the size of Australia?")
+async def generate(sentPrompt: str = Body(embed=True)):
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=sentPrompt,
+    temperature=1,
+    max_tokens=70,
+    top_p=1,
+    frequency_penalty=2,
+    presence_penalty=2
+)
+    return response
+
+# async def generate():
+#     return ("-Did you know that the globe is divided into 24 time zones? \n-Or that there are 196 countries in the world? \n-How about that Antarctica is almost twice the size of Australia?")
