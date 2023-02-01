37ee8da9f44b7b87fdf6449adb39d1dc776297a8
Aiden Zegil <aiden.zegil@tenet.com>
2023-01-31 23:21:01 -0500

    make it better

--- app/api/tweet_gen_routes.py
+++ app/api/tweet_gen_routes.py
@@ -9,14 +9,6 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-# @router.post("/")
-# def generate():
-#     return (
-#     openai.Completion.create(
-#     engine="text-davinci-002",
-#     prompt="Say this is a test",
-#     max_tokens=5))
-
 
 @router.post("/")
 async def generate(sentPrompt: str = Body(embed=True)):
@@ -24,12 +16,9 @@ async def generate(sentPrompt: str = Body(embed=True)):
     engine="text-davinci-002",
     prompt=sentPrompt,
     temperature=1,
-    max_tokens=70,
+    max_tokens=2000,
     top_p=1,
     frequency_penalty=2,
     presence_penalty=2
 )
     return response
-
-# async def generate():
-#     return ("-Did you know that the globe is divided into 24 time zones? \n-Or that there are 196 countries in the world? \n-How about that Antarctica is almost twice the size of Australia?")
